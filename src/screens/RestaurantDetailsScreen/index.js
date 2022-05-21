d0869fb07a0b48903157b5616ed0363be667ad8c
uch2ha <sinyavskiy.d@gmail.com>
2022-05-21 13:47:09 +0300

    RestaurantDetailsScreen

--- src/screens/RestaurantDetailsScreen/index.js
+++ src/screens/RestaurantDetailsScreen/index.js
@@ -5,10 +5,16 @@ import DishListItem from "../../components/DishListItem";
 import Header from "./Header";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "./styles";
+import { useRoute, useNavigation } from "@react-navigation/native";
 
 const restaurant = restaurants[0];
 
 const RestaurantDetailsPage = () => {
+   const route = useRoute();
+   const navigation = useNavigation();
+
+   const id = route.params.id;
+
    return (
       <View style={styles.page}>
          <StatusBar style="light" />
@@ -17,8 +23,10 @@ const RestaurantDetailsPage = () => {
             ListHeaderComponent={() => <Header restaurant={restaurant} />}
             data={restaurant.dishes}
             renderItem={({ item }) => <DishListItem dish={item} />}
+            keyExtractor={(item) => item.name}
          />
          <Ionicons
+            onPress={() => navigation.goBack()}
             name="arrow-back-circle"
             size={45}
             color="white"
